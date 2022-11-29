f7df6ea57b4db6d32a889eeebb6f5714b69907e2
theforbiddenpool <contact@theforbiddenpool.com>
2022-11-29 11:30:32 +0100

    initial commit

---
+++ frontend/.eslintrc.cjs
@@ -0,0 +1,12 @@
+/* eslint-env node */
+module.exports = {
+  root: true,
+  extends: [
+    'plugin:vue/vue3-essential',
+    '@vue/eslint-config-airbnb',
+    'eslint:recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+};
